e4463a833305e76894493ca6f95bb1ba7545a67c
jsectools <114839502+jsectools@users.noreply.github.com>
2024-01-09 13:51:13 +0100

    Add files via upload

---
+++ Export_Machines_and_convert_to_IP.ps1
@@ -0,0 +1,63 @@
+$information = @"
+---------------------------------------------
+== Export_Machines_and_convert_to_IP
+
+Author:: Jose Manuel Mendez
+Copyright:: Copyright (c) 2024
+All rights of use reserved. It is not allowed to copy or use this script without explicit permission
+
+# Note: You will need to replace de value of "Domain" variable according to your domain Also you will need to replace "SearchBase" variable value according to the OU you want to search for.
+
+---------------------------------------------
+
+
+"@
+
+
+Clear-Host
+Write-Host $information
+
+$Domain = "<domain.com>"  # Change acording to your domain
+$LogonServer = $env:LOGONSERVER -replace '\\\\', ''
+$DC = $LogonServer + "." + $Domain
+$date = (Get-Date).ToString("yyyyMMdd")
+$path = "C:\Temp\"
+
+
+$SearchBase_Type0 = "OU=subtree0,OU=Tree0,DC=domain,DC=com"
+$SearchBase_Type1 = "OU=subtree1,OU=Tree1,DC=domain,DC=com"
+$OutputFile_Type0 = $path + $date + "_machines_Type0.csv"
+$OutputFile_Type1 = $path + $date + "_machines_Type1.csv"
+$OutputFile_Type1 = "C:\Temp\machines_Type1.csv"
+
+New-Item -ItemType File -Path $OutputFile_Type0 -Force > $null
+New-Item -ItemType File -Path $OutputFile_Type1 -Force > $null
+
+$names_Type0 = Get-ADComputer -Server $DC -Filter * -SearchBase $SearchBase_Type0 | Select-Object  Name 
+foreach ($name in $names_Type0) {
+    $name.Name = $name.Name + "." + $Domain
+    $ip = (Resolve-DnsName -Name $name.Name).IPAddress
+    $name | Add-Member -NotePropertyName IPAddress -NotePropertyValue $ip
+}
+$names_Type0 | Export-Csv $OutputFile_Type0 -Encoding UTF8 -delimiter '|' -NoTypeInformation
+
+$names_Type1 = Get-ADComputer -Server $DC -Filter * -SearchBase $SearchBase_Type1 | Select-Object  Name 
+foreach ($name in $names_Type1) {
+    $name.Name = $name.Name + "." + $Domain
+    $ip = (Resolve-DnsName -Name $name.Name).IPAddress
+    $name | Add-Member -NotePropertyName IPAddress -NotePropertyValue $ip
+}
+$names_Type1 | Export-Csv $OutputFile_Type1 -Encoding UTF8 -delimiter '|' -NoTypeInformation
+
+
+
+# Ask the user if they want to perform a search at the end
+$search = Read-Host "Do you want to search in the names? (Y/N)" 
+
+if ($search -match "^[Yy]") {
+    $textSearch = Read-Host "Enter the text to search in the names"
+    $names_Type0 | Where-Object { $_.Name -like "*$textSearch*" } | ForEach-Object { Write-host ($_.Name + "`n`tMachine Type 0 `n`tAD Route : " + $SearchBase_Type0)}
+    $names_Type1 | Where-Object { $_.Name -like "*$textSearch*" } | ForEach-Object { Write-Host ($_.Name + "`n`tMachine Type 1 `n`tAD Route : " + $SearchBase_Type1)}
+} else {
+    Write-Host "`nGoodbye!"
+}
---
+++ TODO.md
@@ -0,0 +1,10 @@
+### Todo
+
+- [ ] Review the execution and use cases when executed in a Computer enrolled on AzureAD and not in OnPremise AD
+- [ ] Ask the user in the execution for the domain so the user don't have to hardcode it in the code
+- [ ] Ask the user in the execution for the OU to search so the user don't have to hardcode it in the code
+- [ ] Recursively ask the user to search in the results
+
+### Done ✓
+
+- [x] Execution on Computer enrolled in OnPremise AD  
